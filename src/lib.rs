7e88ed839e418249ae20722b8a6eb6b5aa835a65
Mirko-A <adzicmirko97@gmail.com>
2025-05-14 09:16:04 +0200

    chore: review findings

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ use axum::{
     Router,
     http::StatusCode,
 };
-use axum_server;
 use serde::{Deserialize, Serialize};
 use sqlx::{sqlite::SqlitePoolOptions, Pool, Sqlite, FromRow};
 use tokio::task::JoinHandle;
@@ -27,8 +26,9 @@ struct HashOnly {
     hash: String,
 }
 
+/// Simulated blockchain block.
 #[derive(Debug, Serialize, Deserialize, FromRow)]
-struct Block {                     // simulated blockchain block
+struct Block {                     
     index_id: u64,                 // position in the chain
     timestamp: u64,                // time when block is made 
     transactions: String,          // transactions
@@ -38,19 +38,15 @@ struct Block {                     // simulated blockchain block
 }
 
 pub async fn blockchain_client_http_server_main () -> JoinHandle<()> {
-
-    match std::fs::create_dir_all("./data") {
-        Ok(_) => tracing::info!("Data base directory created or already exists."),
-        Err(e) => tracing::info!("Error creating directory: {}/n", e),
-    }
+    std::fs::create_dir_all("./data").expect("Creating `data` should not fail");
 
     let db_path = std::env::current_dir().unwrap().join("data/blockchain.db");
-    tracing::debug!("Attempting to connect to database at: {}\n", db_path.display());
+    tracing::debug!("Attempting to connect to database at: {}", db_path.display());
 
     let db = SqlitePoolOptions::new()
         .connect(db_path.to_str().unwrap())
         .await
-        .expect("Failed to connect to SQLite\n");
+        .expect("Failed to connect to SQLite");
 
     // Create table if it doesn't exist
     sqlx::query(
@@ -76,11 +72,11 @@ pub async fn blockchain_client_http_server_main () -> JoinHandle<()> {
 
     let app = Router::new()
         .route("/ws", any(handler_websocket))
-        .with_state(app_state.clone())
-        .route("/hash", get(handler_http)).with_state(app_state);
+        .route("/hash", get(handler_http))
+        .with_state(app_state.clone());
 
     let addr = "127.0.0.1:7878".parse().unwrap();
-    tracing::info!("blockchain_client_server listening on ws://{}\n", addr);
+    tracing::info!("blockchain_client_server listening on ws://{}", addr);
 
     let server_handle = tokio::spawn(async move {
         axum_server::Server::bind(addr)
@@ -99,35 +95,36 @@ async fn handler_websocket(ws: WebSocketUpgrade, State(state): State<AppState>) 
 async fn handle_socket(socket: WebSocket, state: AppState) {
     let (_, mut receiver) = socket.split();
 
-    tracing::info!("WebSocket connection established!\n");
+    tracing::info!("WebSocket connection established!");
 
     while let Some(Ok(msg)) = receiver.next().await {
         if let Message::Text(text) = msg {
-            tracing::debug!("Raw message: {}", text);
-            tracing::debug!("Raw message: {:?}", text.as_bytes());
+            tracing::debug!(%text, "Raw message");
+            let bytes = text.as_bytes();
+            tracing::debug!(?bytes, "Raw message");
             match serde_json::from_str::<Block>(&text) {
                 Ok(block) => {
-                    tracing::debug!("Received block:\n{:#?}\n", block);
+                    tracing::debug!(?block, "Received block");
 
                     if let Err(err) = sqlx::query(
-                    r#"
+                        r#"
                         INSERT INTO blocks (index_id, timestamp, transactions, nonce, hash, previous_hash)
                         VALUES(?1, ?2, ?3, ?4, ?5, ?6)
                         "#,
                     )
-                    .bind(block.index_id as i64)
+                    .bind(block.index_id.try_into().expect("Conversion error"))
                     .bind(block.timestamp as i64)
                     .bind(block.transactions)
                     .bind(block.nonce as i64)
                     .bind(block.hash)
                     .bind(block.previous_hash)
-                    .execute(&*state.db)
+                    .execute(state.db.as_ref())
                     .await
                     {
-                        tracing::info!("DB insert error: {}\n", err);
+                        tracing::info!("DB insert error: {}", err);
                     }
                 },
-                Err(e) => tracing::info!("Failed to parse block {e}\n"),
+                Err(e) => tracing::info!("Failed to parse block {e}"),
             }
         }
     }
